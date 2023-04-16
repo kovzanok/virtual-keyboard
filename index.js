6a22a5e5612fe80543e7e2533590bf0061c4bb92
kovzanok <alexkovzanok@gmail.com>
2023-04-16 18:47:34 +0300

    feat: all keyboard is now generated

--- index.js
+++ index.js
@@ -207,6 +207,868 @@ const keyboardRowsArray = [
       ],
     },
   ],
+  [
+    {
+      name: 'Tab',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main',
+          textContent: 'Tab ↹',
+        },
+      ],
+    },
+    {
+      name: 'KeyQ',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'q',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'Q',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'й',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Й',
+        },
+      ],
+    },
+    {
+      name: 'KeyW',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'w',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'W',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'ц',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Ц',
+        },
+      ],
+    },
+    {
+      name: 'KeyE',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'e',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'E',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'у',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'У',
+        },
+      ],
+    },
+    {
+      name: 'KeyR',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'r',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'R',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'к',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'К',
+        },
+      ],
+    },
+    {
+      name: 'KeyT',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 't',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'T',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'е',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Е',
+        },
+      ],
+    },
+    {
+      name: 'KeyY',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'y',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'Y',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'н',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Н',
+        },
+      ],
+    },
+    {
+      name: 'KeyU',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'u',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'U',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'г',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Г',
+        },
+      ],
+    },
+    {
+      name: 'KeyI',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'i',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'I',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'ш',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Ш',
+        },
+      ],
+    },
+    {
+      name: 'KeyO',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'o',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'O',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'щ',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Щ',
+        },
+      ],
+    },
+    {
+      name: 'KeyP',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'p',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'P',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'з',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'З',
+        },
+      ],
+    },
+    {
+      name: 'BracketLeft',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: '[',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: '{',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'х',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Х',
+        },
+      ],
+    },
+    {
+      name: 'BracketRight',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: ']',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: '}',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'ъ',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Ъ',
+        },
+      ],
+    },
+    {
+      name: 'Backslash',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main',
+          textContent: '\\',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: '|',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: '/',
+        },
+      ],
+    },
+    {
+      name: 'Delete',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main',
+          textContent: 'Delete',
+        },
+      ],
+    },
+  ],
+  [
+    {
+      name: 'CapsLock',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main',
+          textContent: 'Caps Lock',
+        },
+      ],
+    },
+    {
+      name: 'KeyA',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'a',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'A',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'ф',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Ф',
+        },
+      ],
+    },
+    {
+      name: 'KeyS',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 's',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'S',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'ы',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Ы',
+        },
+      ],
+    },
+    {
+      name: 'KeyD',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'd',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'D',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'в',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'В',
+        },
+      ],
+    },
+    {
+      name: 'KeyF',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'f',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'F',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'а',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'А',
+        },
+      ],
+    },
+    {
+      name: 'KeyG',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'g',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'G',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'п',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'П',
+        },
+      ],
+    },
+    {
+      name: 'KeyH',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'h',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'H',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'р',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Р',
+        },
+      ],
+    },
+    {
+      name: 'KeyJ',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'j',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'J',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'о',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'О',
+        },
+      ],
+    },
+    {
+      name: 'KeyK',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'k',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'K',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'л',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Л',
+        },
+      ],
+    },
+    {
+      name: 'KeyL',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'l',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'L',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'д',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Д',
+        },
+      ],
+    },
+    {
+      name: 'Semicolon',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: ';',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: ':',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'ж',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Ж',
+        },
+      ],
+    },
+    {
+      name: 'Quote',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: '\'',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: '"',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'э',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Э',
+        },
+      ],
+    },
+    {
+      name: 'Enter',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main',
+          textContent: '↵ Enter',
+        },
+      ],
+    },
+  ],
+  [
+    {
+      name: 'ShiftLeft',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main',
+          textContent: '⇧ Shift',
+        },
+      ],
+    },
+    {
+      name: 'KeyZ',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'z',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'Z',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'я',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Я',
+        },
+      ],
+    },
+    {
+      name: 'KeyX',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'x',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'X',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'ч',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Ч',
+        },
+      ],
+    },
+    {
+      name: 'KeyC',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'c',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'C',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'с',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'С',
+        },
+      ],
+    },
+    {
+      name: 'KeyV',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'v',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'V',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'м',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'М',
+        },
+      ],
+    },
+    {
+      name: 'KeyB',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'b',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'B',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'и',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'И',
+        },
+      ],
+    },
+    {
+      name: 'KeyN',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'n',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'N',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'т',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Т',
+        },
+      ],
+    },
+    {
+      name: 'KeyM',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'm',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: 'M',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'ь',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Ь',
+        },
+      ],
+    },
+    {
+      name: 'Comma',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: ',',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: '<',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'б',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Б',
+        },
+      ],
+    },
+    {
+      name: 'Period',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: '.',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: '>',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: 'ю',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: 'Ю',
+        },
+      ],
+    },
+    {
+      name: 'Slash',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: '/',
+        },
+        {
+          elementClassName: 'key-shift eng hidden',
+          textContent: '?',
+        },
+        {
+          elementClassName: 'key-main rus hidden',
+          textContent: '.',
+        },
+        {
+          elementClassName: 'key-shift rus hidden',
+          textContent: ',',
+        },
+      ],
+    },
+    {
+      name: 'ArrowUp',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main',
+          textContent: '↑',
+        },
+      ],
+    },
+    {
+      name: 'ShiftRight',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main',
+          textContent: '⇧ Shift',
+        },
+      ],
+    },
+  ],
+  [
+    {
+      name: 'ControlLeft',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main',
+          textContent: 'Ctrl',
+        },
+      ],
+    },
+    {
+      name: 'MetaLeft',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main',
+          textContent: 'Win',
+        },
+      ],
+    },
+    {
+      name: 'AltLeft',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main',
+          textContent: 'Alt',
+        },
+      ],
+    },
+    {
+      name: 'Space',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main',
+          textContent: '',
+        },
+      ],
+    },
+    {
+      name: 'AltRight',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main eng',
+          textContent: 'Alt',
+        },
+      ],
+    },
+    {
+      name: 'ArrowLeft',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main',
+          textContent: '←',
+        },
+      ],
+    },
+    {
+      name: 'ArrowDown',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main',
+          textContent: '↓',
+        },
+      ],
+    },
+    {
+      name: 'ArrowRight',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main',
+          textContent: '→',
+        },
+      ],
+    },
+    {
+      name: 'ControlRight',
+      keyInnerElements: [
+        {
+          elementClassName: 'key-main',
+          textContent: 'Ctrl',
+        },
+      ],
+    },
+  ],
 ];
 
 function renderKeyboardKey(keyboardKeyObj) {
